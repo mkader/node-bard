03fd5c42d8bc977621d68e207425db1182d720ef
DayNightSoft <daynightsoft@gmail.com>
2023-05-03 05:40:31 +0000

    bard api

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ import fetch from "node-fetch";
 
 const BARD_URL =
   "https://bard.google.com/_/BardChatUi/data/assistant.lamda.BardFrontendService/StreamGenerate";
-const Secure1PSID = process.env.Secure1PSID;
-const AT_KEY = process.env.AT_KEY;
-const PROMPT = "What year did we go to the moon?";
+const Secure1PSID = "WAjZ8rIwggZ_d4Vn3yHTX22k0aDzWDCPZlL7Vd4OAv3JUubSWLLGSAo3ldXQFg_EbxP-gA.";
+const AT_KEY = "ABi_lZj89y8BilcWw3M9q5kypUb8:1683091283277";
+const PROMPT = "job_post : We are looking for a software engineer with experience in Python and machine learning. The candidate should have a strong understanding of data structures and algorithms, and be able to work well in a team environment. resume :I am a software engineer with 5 years of experience. I have extensive experience in Python and machine learning, and have worked on several projects involving data analysis and visualization. I am familiar with a wide range of data structures and algorithms, and have a proven track record of working well in team environments. Generate 5 fair, unbiased, and relevant interview questions for a software engineer position based on the following job post and resume";
 
 const params = new URLSearchParams({
   bl: "boq_assistant-bard-web-server_20230419.00_p1",
@@ -47,6 +47,8 @@ const requestOptions = {
 
 const request = await fetch(`${BARD_URL}?${params}`, requestOptions);
 const response = await request.text();
+
+//console.log(response);
 
 const output = JSON.parse(response.split(/\r?\n/)[3])[0][2];
 const content = JSON.parse(output)[0][0];
